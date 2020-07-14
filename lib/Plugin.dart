02595c88623bfff6f4242fc1d9f0ffaf75a07605
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2020-07-15 04:26:35 +0530

    videoroom publishing now works,example updated accordingly

--- lib/Plugin.dart
+++ lib/Plugin.dart
@@ -8,11 +8,16 @@ class Plugin {
   Function(dynamic, dynamic) onMessage;
   Function(dynamic) onLocalStream;
   Function(dynamic) onRemoteStream;
+  Function(dynamic) onIceConnectionState;
+  Function(dynamic, dynamic) onWebRTCState;
+  Function() onDetached;
+  Function(dynamic, dynamic) onMediaState;
 
   Plugin(
       {this.plugin,
       this.onSuccess,
       this.onError,
+      this.onWebRTCState,
       this.onMessage,
       this.onLocalStream,
       this.onRemoteStream});
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+
 class RTCIceServer {
   String username;
-  String credentials;
-  String urls;
+  String credential;
+  String url;
 
 //<editor-fold desc="Data Methods" defaultstate="collapsed">
 
   RTCIceServer({
-    this.username,
-    this.credentials,
-    this.urls,
+    @required this.username,
+    @required this.credential,
+    @required this.url,
   });
 
   @override
@@ -19,34 +21,46 @@ class RTCIceServer {
       (other is RTCIceServer &&
           runtimeType == other.runtimeType &&
           username == other.username &&
-          credentials == other.credentials &&
-          urls == other.urls);
+          credential == other.credential &&
+          url == other.url);
 
   @override
-  int get hashCode => username.hashCode ^ credentials.hashCode ^ urls.hashCode;
+  int get hashCode => username.hashCode ^ credential.hashCode ^ url.hashCode;
 
   @override
   String toString() {
     return 'RTCIceServer{' +
         ' username: $username,' +
-        ' credentials: $credentials,' +
-        ' urls: $urls,' +
+        ' credential: $credential,' +
+        ' url: $url,' +
         '}';
+  }
+
+  RTCIceServer copyWith({
+    String username,
+    String credential,
+    String url,
+  }) {
+    return new RTCIceServer(
+      username: username ?? this.username,
+      credential: credential ?? this.credential,
+      url: url ?? this.url,
+    );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'username': this.username,
-      'credentials': this.credentials,
-      'urls': this.urls,
+      'credential': this.credential,
+      'url': this.url,
     };
   }
 
   factory RTCIceServer.fromMap(Map<String, dynamic> map) {
     return new RTCIceServer(
       username: map['username'] as String,
-      credentials: map['credentials'] as String,
-      urls: map['urls'] as String,
+      credential: map['credential'] as String,
+      url: map['url'] as String,
     );
   }
 

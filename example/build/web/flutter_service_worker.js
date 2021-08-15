d48c55b92518421ae9c5e6a8fc509022718c4e14
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2021-08-16 02:33:32 +0530

    build

--- example/build/web/flutter_service_worker.js
+++ example/build/web/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "d146e2b4fd93945982a6a9de4fe4c975",
-"/": "d146e2b4fd93945982a6a9de4fe4c975",
+  "index.html": "6b6aed2746880276d23fd45b0c728226",
+"/": "6b6aed2746880276d23fd45b0c728226",
 "assets/NOTICES": "0c06a15b408738ed06ac97611b8ee409",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",

1c64555c34adb756c226da370a7fec3ae0d1d1da
shivansh <shivansh@zopper.com>
2021-12-19 21:28:33 +0530

    video room all important events configured and done cleanup on user pending
    Former-commit-id: 6e98332404a43729f63e9dfd7b0e29810b2683b8 [formerly 375bfbbd4b17c6820dca6006abc8187b2f950c9e [formerly 02212cef35c830eaab0fd73ddef09e172a98c2d4]]
    Former-commit-id: 2db65bb21ffc3fc19501e2ad14063c6b109cbfbb
    Former-commit-id: d5d014c0cfcbbdff02bb6f07305c552ea91d98df

---
+++ lib/interfaces/VideoRoom/Events/video_room_configured.dart
@@ -0,0 +1,25 @@
+import 'package:janus_client/interfaces/interfaces.dart';
+
+class VideoRoomConfigured extends VideoRoomEvent {
+  VideoRoomConfigured({
+    videoroom,
+    this.configured,
+  }) {
+    super.videoroom = videoroom;
+    super.room = 0;
+  }
+
+  VideoRoomConfigured.fromJson(dynamic json) {
+    videoroom = json['videoroom'];
+    configured = json['configured'];
+  }
+
+  String? configured;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['videoroom'] = videoroom;
+    map['configured'] = configured;
+    return map;
+  }
+}

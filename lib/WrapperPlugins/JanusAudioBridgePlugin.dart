54b5ac38ab52222b812184b22ec1a31ae27a5d72
shivansh <shivansh@zopper.com>
2021-12-20 01:32:59 +0530

    audiobridge wrapper plugin development underway critical methods done
    Former-commit-id: de5952205ae1e4ccc19bfffd485c13797cf74126 [formerly ac25312d5af6116c63c9e04e35e4428e4e95193b [formerly d2f876f578b3d60b0f6b93acb94b087135600871]]
    Former-commit-id: 15a8c82bb0c01c07f9d8cf27b013167080d627a8
    Former-commit-id: 6c41ae69cd8535f5540ff25a4ed84db01a018947

--- lib/WrapperPlugins/JanusAudioBridgePlugin.dart
+++ lib/WrapperPlugins/JanusAudioBridgePlugin.dart
@@ -1,7 +1,68 @@
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:janus_client/JanusClient.dart';
 
 class JanusAudioBridgePlugin extends JanusPlugin {
   JanusAudioBridgePlugin({handleId, context, transport, session}) : super(context: context, handleId: handleId, plugin: JanusPlugins.AUDIO_BRIDGE, session: session, transport: transport);
+
+  Future<AudioRoomCreatedResponse> createRoom(int roomId,
+      {bool permanent = true,
+      String? description,
+      String? secret,
+      String? pin,
+      bool? is_private,
+      bool? audiolevel_ext,
+      bool? audiolevel_event,
+      int? audio_active_packets,
+      int? audio_level_average,
+      List<String>? allowed,
+      bool? record,
+      String? record_file,
+      String? record_dir,
+      String? default_prebuffering,
+      String? allow_rtp_participants,
+      int? sampling_rate,
+      List<String>? groups,
+      bool? spatial_audio}) async {
+    var payload = {
+      "request": "create",
+      "room": roomId,
+      "permanent": permanent,
+      if (description != null) "description": description,
+      if (secret != null) "secret": secret,
+      if (pin != null) "pin": pin,
+      if (is_private != null) "is_private": is_private,
+      if (allowed != null) "allowed": allowed,
+      if (sampling_rate != null) "sampling_rate": sampling_rate,
+      if (spatial_audio != null) "spatial_audio": spatial_audio,
+      if (audiolevel_ext != null) "audiolevel_ext": audiolevel_ext,
+      if (audiolevel_event != null) "audiolevel_event": audiolevel_event,
+      if (audio_active_packets != null) "audio_active_packets": audio_active_packets,
+      if (audio_level_average != null) "audio_level_average": audio_level_average,
+      if (default_prebuffering != null) "default_prebuffering": default_prebuffering,
+      if (record != null) "record": record,
+      if (record_file != null) "record_file": record_file,
+      if (record_dir != null) "record_dir": record_dir,
+      if (allow_rtp_participants != null) "allow_rtp_participants": allow_rtp_participants,
+      if (groups != null) "groups": groups
+    };
+    Map data = await this.send(data: payload);
+    return AudioRoomCreatedResponse.fromJson(data);
+  }
+
+  Future<dynamic> editRoom(int roomId, {String? secret, String? newDescription, String? newSecret, String? newPin, bool? newIsPrivate, bool? permanent}) async {
+    var payload = {
+      "request": "edit",
+      "room": roomId,
+      if (secret != null) "secret": secret,
+      if (newDescription != null) "new_description": newDescription,
+      if (newSecret != null) "new_secret": newSecret,
+      if (newPin != null) "new_pin": newPin,
+      if (newIsPrivate != null) "new_is_private": newIsPrivate,
+      if (permanent != null) "permanent": permanent
+    };
+    Map data = await this.send(data: payload);
+    return AudioRoomCreatedResponse.fromJson(data);
+  }
 
   Future<void> joinRoom(int roomId,
       {String? id,
@@ -41,5 +102,39 @@ class JanusAudioBridgePlugin extends JanusPlugin {
       if (filename != null) "filename": filename
     };
     Map data = await this.send(data: payload);
+  }
+
+  Future<void> configure({bool? muted, String? display, int? prebuffer, int? quality, int? volume, int? spatial_position, bool? record, String? filename, String? group}) async {
+    var payload = {
+      "request": "configure",
+      if (muted != null) "muted": muted,
+      if (display != null) "display": display,
+      if (prebuffer != null) "prebuffer": prebuffer,
+      if (quality != null) "quality": quality,
+      if (volume != null) "volume": volume,
+      if (spatial_position != null) "spatial_position": spatial_position,
+      if (record != null) "record": record,
+      if (filename != null) "filename": filename,
+      if (group != null) "group": group
+    };
+    RTCSessionDescription? offer = await this.createOffer(videoSend: false, videoRecv: false, audioSend: true, audioRecv: false);
+    Map data = await this.send(data: payload, jsep: offer);
+  }
+
+  bool _onCreated = false;
+
+  @override
+  void onCreate() {
+    super.onCreate();
+    if (!_onCreated) {
+      _onCreated = true;
+      messages?.listen((event) {
+        TypedEvent<JanusEvent> typedEvent = TypedEvent<JanusEvent>(event: JanusEvent.fromJson(event.event), jsep: event.jsep);
+        if (typedEvent.event.plugindata?.data["audiobridge"] == "joined") {
+          typedEvent.event.plugindata?.data = AudioBridgeJoinedEvent.fromJson(typedEvent.event.plugindata?.data);
+          typedMessagesSink?.add(typedEvent);
+        }
+      });
+    }
   }
 }
--- lib/interfaces/VideoRoom/Events/video_room_new_publisher_event.dart
+++ lib/interfaces/VideoRoom/Events/video_room_new_publisher_event.dart
@@ -34,42 +34,6 @@ class VideoRoomNewPublisherEvent extends VideoRoomEvent {
   }
 }
 
-// class Publishers {
-//   Publishers({
-//       this.id,
-//       this.display,
-//       this.streams,
-//       this.talking,});
-//
-//   Publishers.fromJson(dynamic json) {
-//     id = json['id'];
-//     display = json['display'];
-//     if (json['streams'] != null) {
-//       streams = [];
-//       json['streams'].forEach((v) {
-//         streams?.add(Streams.fromJson(v));
-//       });
-//     }
-//     talking = json['talking'];
-//   }
-//   int? id;
-//   String? display;
-//   List<Streams>? streams;
-//   bool? talking;
-//
-//   Map<String, dynamic> toJson() {
-//     final map = <String, dynamic>{};
-//     map['id'] = id;
-//     map['display'] = display;
-//     if (streams != null) {
-//       map['streams'] = streams?.map((v) => v.toJson()).toList();
-//     }
-//     map['talking'] = talking;
-//     return map;
-//   }
-//
-// }
-
 class PublisherStream {
   bool? simulcast;
   bool? svc;
@@ -133,7 +97,6 @@ class PublisherStream {
 }
 
 class Streams extends BaseStream {
-
   Streams.fromJson(dynamic json) {
     type = json['type'];
     mindex = json['mindex'];
@@ -169,91 +132,4 @@ class Streams extends BaseStream {
     map['talking'] = talking;
     return map;
   }
-
-// //<editor-fold desc="Data Methods">
-//
-//   Streams({
-//     this.disabled,
-//     this.codec,
-//     this.description,
-//     this.moderated,
-//     this.simulcast,
-//     this.svc,
-//     this.talking,
-//   });
-//
-//   @override
-//   bool operator ==(Object other) =>
-//       identical(this, other) ||
-//       (other is Streams &&
-//           runtimeType == other.runtimeType &&
-//           disabled == other.disabled &&
-//           codec == other.codec &&
-//           description == other.description &&
-//           moderated == other.moderated &&
-//           simulcast == other.simulcast &&
-//           svc == other.svc &&
-//           talking == other.talking);
-//
-//   @override
-//   int get hashCode => disabled.hashCode ^ codec.hashCode ^ description.hashCode ^ moderated.hashCode ^ simulcast.hashCode ^ svc.hashCode ^ talking.hashCode;
-//
-//   @override
-//   String toString() {
-//     return 'Streams{' +
-//         ' disabled: $disabled,' +
-//         ' codec: $codec,' +
-//         ' description: $description,' +
-//         ' moderated: $moderated,' +
-//         ' simulcast: $simulcast,' +
-//         ' svc: $svc,' +
-//         ' talking: $talking,' +
-//         '}';
-//   }
-//
-//   Streams copyWith({
-//     bool? disabled,
-//     String? codec,
-//     String? description,
-//     bool? moderated,
-//     bool? simulcast,
-//     bool? svc,
-//     bool? talking,
-//   }) {
-//     return Streams(
-//       disabled: disabled ?? this.disabled,
-//       codec: codec ?? this.codec,
-//       description: description ?? this.description,
-//       moderated: moderated ?? this.moderated,
-//       simulcast: simulcast ?? this.simulcast,
-//       svc: svc ?? this.svc,
-//       talking: talking ?? this.talking,
-//     );
-//   }
-//
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'disabled': this.disabled,
-//       'codec': this.codec,
-//       'description': this.description,
-//       'moderated': this.moderated,
-//       'simulcast': this.simulcast,
-//       'svc': this.svc,
-//       'talking': this.talking,
-//     };
-//   }
-//
-//   factory Streams.fromMap(Map<String, dynamic> map) {
-//     return Streams(
-//       disabled: map['disabled'] as bool,
-//       codec: map['codec'] as String,
-//       description: map['description'] as String,
-//       moderated: map['moderated'] as bool,
-//       simulcast: map['simulcast'] as bool,
-//       svc: map['svc'] as bool,
-//       talking: map['talking'] as bool,
-//     );
-//   }
-//
-// //</editor-fold>
 }

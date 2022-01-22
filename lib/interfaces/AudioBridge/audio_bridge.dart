c0e9b33245c97be71b49fcfe1516a92772363498
shivansh <shivansh@zopper.com>
2022-01-22 19:58:33 +0530

    audiobridge typed wrapper plugin with rich documentation and preliminary unit-test cases

--- lib/interfaces/AudioBridge/audio_bridge.dart
+++ lib/interfaces/AudioBridge/audio_bridge.dart
@@ -1,2 +1,4 @@
 export 'audio_room_created_response.dart';
 export 'Events/events.dart';
+export 'rtp_forward_stopped.dart';
+export 'rtp_forwarder_created.dart';
---
+++ lib/interfaces/AudioBridge/rtp_forward_stopped.dart
@@ -0,0 +1,24 @@
+class RtpForwardStopped {
+  RtpForwardStopped({
+      this.audiobridge, 
+      this.room, 
+      this.streamId,});
+
+  RtpForwardStopped.fromJson(dynamic json) {
+    audiobridge = json['audiobridge'];
+    room = json['room'];
+    streamId = json['stream_id'];
+  }
+  String? audiobridge;
+  int? room;
+  int? streamId;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['audiobridge'] = audiobridge;
+    map['room'] = room;
+    map['stream_id'] = streamId;
+    return map;
+  }
+
+}

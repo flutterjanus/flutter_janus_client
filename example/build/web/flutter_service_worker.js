45778f319fb3163b64e00bd39e8c8b7703f50ee2
shivansh <shivansh@zopper.com>
2022-02-15 23:45:50 +0530

    screen share finalized

--- example/build/web/flutter_service_worker.js
+++ example/build/web/flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "b0969f225be1e4f66316ff62b0d0208a",
 "index.html": "b71bd900c304674871e2a167d9c801a8",
 "/": "b71bd900c304674871e2a167d9c801a8",
-"main.dart.js": "f920122ec2670b38a4cef761370f23ab",
+"main.dart.js": "4f00b331b692f36b6782271c60a27358",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

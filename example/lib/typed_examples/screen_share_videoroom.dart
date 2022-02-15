332de4e95797cc0f75e32b3d732c32bae4be66ce
shivansh <shivansh@zopper.com>
2022-02-16 00:31:42 +0530

    full screen support
    Former-commit-id: 8aec44f14f8ff988c6a8ca17020216b832d5c53f [formerly ea62ec9e0aaa9972bb313abf221e3dfeb575f4b4]
    Former-commit-id: 7c0becc94f73ecf87aeb6f3620c4918cb76476e7
    Former-commit-id: 9aca77c6d11070176d9b42f26d260040a875b24c

--- example/lib/typed_examples/screen_share_videoroom.dart
+++ example/lib/typed_examples/screen_share_videoroom.dart
@@ -27,31 +27,27 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
   Map<int, dynamic> feeds = {};
   Map<String, int> subStreams = {};
   Map<int, MediaStream?> mediaStreams = {};
-  bool roomJoined=false;
-  bool screenSharing=false;
+  bool roomJoined = false;
+  bool screenSharing = false;
+  RemoteStream? zoomStream;
 
   @override
   void didChangeDependencies() async {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
-    initialize();
+    await initialize();
   }
 
   initialize() async {
     ws = WebSocketJanusTransport(url: servermap['janus_ws']);
-    j = JanusClient(transport: ws, isUnifiedPlan: true, iceServers: [
-      RTCIceServer(
-          urls: "stun:stun1.l.google.com:19302", username: "", credential: "")
-    ]);
+    j = JanusClient(transport: ws, isUnifiedPlan: true, iceServers: [RTCIceServer(urls: "stun:stun1.l.google.com:19302", username: "", credential: "")]);
     session = await j.createSession();
     plugin = await session.attach<JanusVideoRoomPlugin>();
   }
 
   subscribeTo(List<Map<String, dynamic>> sources) async {
     if (sources.length == 0) return;
-    var streams = (sources)
-        .map((e) => PublisherStream(mid: e['mid'], feed: e['feed']))
-        .toList();
+    var streams = (sources).map((e) => PublisherStream(mid: e['mid'], feed: e['feed'])).toList();
     if (remoteHandle != null) {
       await remoteHandle?.subscribeToStreams(streams);
       return;
@@ -68,8 +64,7 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
             subStreams[element.mid!] = element.feedId!;
           }
           // to avoid duplicate subscriptions
-          if (subscriptions[element.feedId] == null)
-            subscriptions[element.feedId] = {};
+          if (subscriptions[element.feedId] == null) subscriptions[element.feedId] = {};
           subscriptions[element.feedId][element.mid] = true;
         });
         print('substreams');
@@ -93,8 +88,7 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
         }
         if (event.track != null && event.flowing == true) {
           remoteStreams[feedId]?.video.addTrack(event.track!);
-          remoteStreams[feedId]?.videoRenderer.srcObject =
-              remoteStreams[feedId]?.video;
+          remoteStreams[feedId]?.videoRenderer.srcObject = remoteStreams[feedId]?.video;
           remoteStreams[feedId]?.videoRenderer.muted = false;
         }
       }
@@ -103,8 +97,7 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
   }
 
   Future<void> joinRoom() async {
-    myStream = await plugin.initializeMediaDevices(
-        useDisplayMediaDevices: true,mediaConstraints: {"video":true,"audio":true});
+    myStream = await plugin.initializeMediaDevices(useDisplayMediaDevices: true, mediaConstraints: {"video": true, "audio": true});
     RemoteStream mystr = RemoteStream('0');
     await mystr.init();
     mystr.videoRenderer.srcObject = myStream;
@@ -115,12 +108,12 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
       1234,
       displayName: "Shivansh",
     );
-    var transreciever=await plugin.webRTCHandle?.peerConnection?.transceivers;
+    var transreciever = await plugin.webRTCHandle?.peerConnection?.transceivers;
     transreciever?.forEach((element) {
       element.sender.track?.onEnded = () {
         print('screen share ended');
         setState(() {
-          screenSharing=false;
+          screenSharing = false;
         });
       };
     });
@@ -131,11 +124,7 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
         List<Map<String, dynamic>> publisherStreams = [];
         for (Publishers publisher in data.publishers ?? []) {
           for (Streams stream in publisher.streams ?? []) {
-            feedStreams[publisher.id!] = {
-              "id": publisher.id,
-              "display": publisher.display,
-              "streams": publisher.streams
-            };
+            feedStreams[publisher.id!] = {"id": publisher.id, "display": publisher.display, "streams": publisher.streams};
             publisherStreams.add({"feed": publisher.id, ...stream.toJson()});
             if (publisher.id != null && stream.mid != null) {
               subStreams[stream.mid!] = publisher.id!;
@@ -149,11 +138,7 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
       if (data is VideoRoomNewPublisherEvent) {
         List<Map<String, dynamic>> publisherStreams = [];
         for (Publishers publisher in data.publishers ?? []) {
-          feedStreams[publisher.id!] = {
-            "id": publisher.id,
-            "display": publisher.display,
-            "streams": publisher.streams
-          };
+          feedStreams[publisher.id!] = {"id": publisher.id, "display": publisher.display, "streams": publisher.streams};
           for (Streams stream in publisher.streams ?? []) {
             publisherStreams.add({"feed": publisher.id, ...stream.toJson()});
             if (publisher.id != null && stream.mid != null) {
@@ -176,8 +161,8 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
         print('typed event with jsep' + event.jsep.toString());
         await plugin.handleRemoteJsep(event.jsep);
         setState(() {
-          roomJoined=true;
-          screenSharing=true;
+          roomJoined = true;
+          screenSharing = true;
         });
       }
     });
@@ -200,8 +185,7 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
         {feed: id}
       ]
     };
-    if (remoteHandle != null)
-      await remoteHandle?.send(data: {"message": unsubscribe});
+    if (remoteHandle != null) await remoteHandle?.send(data: {"message": unsubscribe});
     this.subscriptions.remove(id);
   }
 
@@ -235,25 +219,25 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
     await remoteHandle?.dispose();
   }
 
-  Future<void>screenShareAgain()async{
-    var transreciever=await plugin.webRTCHandle?.peerConnection?.transceivers;
-    MediaStream stream=await navigator.mediaDevices.getDisplayMedia({"video":true,"audio":true});
-    remoteStreams[0]?.videoRenderer.srcObject=stream;
+  Future<void> screenShareAgain() async {
+    var transreciever = await plugin.webRTCHandle?.peerConnection?.transceivers;
+    MediaStream stream = await navigator.mediaDevices.getDisplayMedia({"video": true, "audio": true});
+    remoteStreams[0]?.videoRenderer.srcObject = stream;
     transreciever?.forEach((element) {
-      element.sender.track?.onMute=(){};
-      if(element.sender.track?.kind=="video"){
+      element.sender.track?.onMute = () {};
+      if (element.sender.track?.kind == "video") {
         stream.getVideoTracks().forEach((track) {
           element.sender.replaceTrack(track);
         });
       }
-      if(element.sender.track?.kind=="audio"){
+      if (element.sender.track?.kind == "audio") {
         stream.getAudioTracks().forEach((track) {
           element.sender.replaceTrack(track);
         });
       }
     });
     setState(() {
-      screenSharing=true;
+      screenSharing = true;
     });
   }
 
@@ -264,17 +248,18 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
           actions: [
             IconButton(
                 icon: Icon(
-                  roomJoined&&screenSharing?Icons.play_circle_outline:Icons.screen_share,
+                  roomJoined && screenSharing ? Icons.play_circle_outline : Icons.screen_share,
                   color: Colors.greenAccent,
                 ),
-                onPressed: roomJoined&&screenSharing?null:() async {
-                  if(!roomJoined){
-                    await this.joinRoom();
-                  }
-                  else if(roomJoined && !screenSharing){
-                    screenShareAgain();
-                  }
-                }),
+                onPressed: roomJoined && screenSharing
+                    ? null
+                    : () async {
+                        if (!roomJoined) {
+                          await this.joinRoom();
+                        } else if (roomJoined && !screenSharing) {
+                          screenShareAgain();
+                        }
+                      }),
             IconButton(
                 icon: Icon(
                   Icons.call_end,
@@ -287,29 +272,60 @@ class _VideoRoomState extends State<TypedScreenShareVideoRoomV2Unified> {
           title: const Text('janus_client'),
         ),
         body: GridView.builder(
-            gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            itemCount:
-            remoteStreams.entries
-                .map((e) => e.value)
-                .toList()
-                .length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemCount: remoteStreams.entries.map((e) => e.value).toList().length,
             itemBuilder: (context, index) {
-              List<RemoteStream> items =
-              remoteStreams.entries.map((e) => e.value).toList();
+              List<RemoteStream> items = remoteStreams.entries.map((e) => e.value).toList();
               RemoteStream remoteStream = items[index];
               return Stack(
                 children: [
-                  RTCVideoView(remoteStream.audioRenderer,
-                      filterQuality: FilterQuality.high,
-                      objectFit:
-                      RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-                      mirror: true),
-                  RTCVideoView(remoteStream.videoRenderer,
-                      filterQuality: FilterQuality.high,
-                      objectFit:
-                      RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
-                      mirror: true)
+                  RTCVideoView(remoteStream.videoRenderer, filterQuality: FilterQuality.high, objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover),
+                  Align(
+                      alignment: Alignment.bottomRight,
+                      child: IconButton(
+                        onPressed: () async {
+                          var dialog;
+                          setState(() {
+                            zoomStream = remoteStream;
+                          });
+                          dialog = await showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  insetPadding: EdgeInsets.zero,
+                                  contentPadding: EdgeInsets.zero,
+                                  content:
+                                  SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                height: MediaQuery.of(context).size.height,
+                                child:Stack(children: [
+                                    Positioned.fill(
+                                        child: RTCVideoView(zoomStream!.videoRenderer, filterQuality: FilterQuality.high, objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitContain),
+                                    ),
+                                    Align(
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Flexible(
+                                              child: IconButton(
+                                            icon: Icon(Icons.close,color:Colors.white),
+                                            onPressed: () {
+                                              Navigator.of(context).pop(dialog);
+                                            },
+                                          ))
+                                        ],
+                                      ),
+                                      alignment: Alignment.topRight,
+                                    )
+                                  ]),
+                                ));
+                              });
+                        },
+                        icon: Icon(Icons.fit_screen, color: Colors.white),
+                        iconSize: 20,
+                        splashRadius: 24,
+                      ))
                 ],
               );
             }));

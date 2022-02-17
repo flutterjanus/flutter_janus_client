4fa713fbc96d251290fe4f92dac23b794f9fb676
shivansh <shivansh@zopper.com>
2022-02-17 18:53:54 +0530

    build

--- example/build/web/flutter_service_worker.js
+++ example/build/web/flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "b0969f225be1e4f66316ff62b0d0208a",
 "index.html": "b71bd900c304674871e2a167d9c801a8",
 "/": "b71bd900c304674871e2a167d9c801a8",
-"main.dart.js": "99d6c8af5bc58eef154fe8672ca859ca",
+"main.dart.js": "8c8cee2d4fc69cd648eece8c0f01c099",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

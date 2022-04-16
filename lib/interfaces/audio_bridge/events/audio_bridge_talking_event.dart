6e9b85e6ac22da7a0cb2ce78c0e000599dc788f6
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2022-04-16 19:28:53 +0530

    flutter_webrtc bump and fix audiobridge talking example

---
+++ lib/interfaces/audio_bridge/events/audio_bridge_talking_event.dart
@@ -0,0 +1,32 @@
+part of janus_client;
+
+class AudioBridgeTalkingEvent extends AudioBridgeEvent {
+  int? userId;
+  bool? isTalking;
+  AudioBridgeTalkingEvent({audiobridge, room, userId, isTalking}) {
+    super.audiobridge = audiobridge;
+    super.room = room;
+    this.userId = userId;
+    this.isTalking = isTalking;
+  }
+
+  AudioBridgeTalkingEvent.fromJson(dynamic json) {
+    audiobridge = json['audiobridge'];
+    room = json['room'];
+    userId = json['id'];
+    isTalking = json['audiobridge'] == 'talking'
+        ? true
+        : json['audiobridge'] == 'stopped-talking'
+            ? false
+            : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['audiobridge'] = audiobridge;
+    map['room'] = room;
+    map['id'] = userId;
+    map['isTalking'] = isTalking;
+    return map;
+  }
+}

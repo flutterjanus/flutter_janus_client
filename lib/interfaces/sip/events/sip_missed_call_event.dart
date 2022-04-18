f5d801b15eca5c0d5a076a0b98b82fa94efd4f6a
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2022-04-19 01:42:45 +0530

    events mapping done in sip wrapper
    Former-commit-id: 857f879abde7559ab0c49d5e7ef4b27a66aea702 [formerly a9bbb288d71daf4a112923311746cdcb0c1932c9]
    Former-commit-id: 3cf3660a4d0da263a249b2629bad203ebb516e12
    Former-commit-id: de1d9010c6aa6f94dcf9bdb9f27406a523416c61

---
+++ lib/interfaces/sip/events/sip_missed_call_event.dart
@@ -0,0 +1,49 @@
+class SipMissedCallEvent {
+  String? sip;
+  String? callId;
+  SipMissedCallEventResult? result;
+
+  SipMissedCallEvent({this.sip, this.callId, this.result});
+
+  SipMissedCallEvent.fromJson(Map<String, dynamic> json) {
+    this.sip = json["sip"];
+    this.callId = json["call_id"];
+    this.result = json["result"] == null
+        ? null
+        : SipMissedCallEventResult.fromJson(json["result"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data["sip"] = this.sip;
+    data["call_id"] = this.callId;
+    if (this.result != null) data["result"] = this.result?.toJson();
+    return data;
+  }
+}
+
+class SipMissedCallEventResult {
+  String? event;
+  String? caller;
+  String? displayname;
+  String? callee;
+
+  SipMissedCallEventResult(
+      {this.event, this.caller, this.displayname, this.callee});
+
+  SipMissedCallEventResult.fromJson(Map<String, dynamic> json) {
+    this.event = json["event"];
+    this.caller = json["caller"];
+    this.displayname = json["displayname"];
+    this.callee = json["callee"];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data["event"] = this.event;
+    data["caller"] = this.caller;
+    data["displayname"] = this.displayname;
+    data["callee"] = this.callee;
+    return data;
+  }
+}

474e9735786159413c292ad59d57d274ed349499
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2025-05-05 03:49:01 +0530

    test

--- example/lib/conf.dart
+++ example/lib/conf.dart
@@ -1,5 +1,5 @@
 Map<String, String> servermap = {
-  'janus_ws': 'ws://107.152.38.126:8188',
+  'janus_ws': 'wss://janus1.januscaler.com/janus/ws',
   'janus_rest': 'https://janus.conf.meetecho.com/janus',
   'servercheap': 'ws://107.152.35.248:8188'
 };
